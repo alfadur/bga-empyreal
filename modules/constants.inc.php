3af9f2cbafb389cb5395d486a3b85dc08b2d9be5
alfadur <mail@none>
2023-11-27 19:12:54 +0300

    define tile coords

--- modules/constants.inc.php
+++ modules/constants.inc.php
@@ -30,3 +30,33 @@ interface State {
 
 interface Globals {
 }
+
+interface HexDirection {
+    const RIGHT = [1, 0];
+    const BOTTOM_RIGHT = [1, 1];
+    const BOTTOM_LEFT = [0, 1];
+    const LEFT = [-1, 0];
+    const TOP_LEFT = [-1, -1];
+    const TOP_RIGHT = [0, -1];
+
+    const ALL = [self::RIGHT, self::BOTTOM_RIGHT, self::BOTTOM_LEFT,
+        self::LEFT, self::TOP_LEFT, self::TOP_RIGHT];
+}
+
+const TILE_COORDS_AB = [
+    [0, 0], [1, 0], [2, 0], [3, 0], [4, 0],
+    [0, 1], [1, 1], [2, 1], [3, 1], [4, 1],
+    [1, 2], [2, 2], [3, 2], [4, 2], [5, 2],
+    [1, 3], [2, 3], [3, 3], [4, 3], [5, 3],
+    [2, 4], [3, 4], [4, 4], [5, 4], [6, 4],
+    [2, 5], [3, 5], [4, 5], [5, 5], [6, 5],
+];
+
+const TILE_COORDS_C = [
+    [0, 0], [1, 0], [2, 0], [3, 0], [4, 0],
+    [1, 1], [2, 1], [3, 1], [4, 1], [5, 1],
+    [1, 2], [2, 2], [3, 2], [4, 2], [5, 2],
+    [2, 3], [3, 3], [4, 3], [5, 3], [6, 3],
+    [2, 4], [3, 4], [4, 4], [5, 4], [6, 4],
+    [3, 5], [4, 5], [5, 5], [6, 5], [7, 5],
+];
